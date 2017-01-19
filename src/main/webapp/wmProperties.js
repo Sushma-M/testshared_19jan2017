8568887a510dc98a7d12b8998446746dd5283613
root <root@093f227bbc1c>
2017-01-19 10:25:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testshared_19jan2017",
+  "homePage" : "Main",
+  "name" : "testshared_19jan2017",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
